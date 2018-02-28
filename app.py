24c50bdc4c74c920910d4a5c337d5ed336971c91
balu <balagopal.u@gmail.com>
2018-03-01 00:44:16 +0800

    added failsafe

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 
+
 #!/usr/bin/env python
 
 import os
@@ -22,17 +23,17 @@ def js_static(filename):
 clips_process = clips_terminal.start()
 
 @route("/")
-@view("questions")
+@view("main")
 def hello():
 	while True:
 		isQn,content = clips_terminal.getContent(clips_process)
 		if isQn:
 			if '(yes/no)' in content:
-				return dict(title='Questions to User',user = 'Balu',question=content, opt1="yes", opt2="no", opt3=None)
+				return dict(question=content, opt1="yes", opt2="no", opt3=None)
 			elif '(1/2)' in content:
-				return dict(title='Questions to User',user = 'Balu',question=content, opt1="1", opt2="2", opt3=None)
+				return dict(question=content, opt1="1", opt2="2", opt3=None)
 			elif '(1/2/3)' in content:
-				return dict(title='Questions to User',user = 'Balu',question=content, opt1="1", opt2="2",opt3="3")
+				return dict(question=content, opt1="1", opt2="2",opt3="3")
 		else:
 			clips_terminal.giveSuggestion(clips_process,content)
 			return suggest(content)
@@ -42,13 +43,6 @@ def recFacts():
 	answer =  request.forms.get('ques')
 	clips_terminal.giveAnswer(clips_process,answer)
 	return hello()
-
-@route("/estimate")
-@view("estimate")
-def suggest(suggestion):
-	clips_process.kill()
-	print 'process killed'
-	return dict(content=suggestion,user = 'Balu')
 
 # -------------------MAIN START----------------------------
 if __name__ == "__main__":
--- clips_terminal.py
+++ clips_terminal.py
@@ -4,24 +4,40 @@ from subprocess import Popen,PIPE
 
 def start():
 	clips_process = Popen('bash', shell=False, stdout=PIPE, stdin=PIPE, stderr=PIPE)
-	clips_process.stdin.write("clips -f clips/main.CLP\n")
+	clips_process.stdin.write("clips -f2 clips/main.CLP\n")
 	return clips_process
 
+def terminate(clips_process):
+	clips_process.terminate();
+
+def restart(clips_process=None):
+	if clips_process != None:
+		terminate(clips_process)
+	return start()
+
 def getContent(clips_process):
+	# print "getContent"
+	
 	content = ""
 	while True:
 		line = clips_process.stdout.readline()
-		content += line
+		content += line.replace('_',' ').replace('/~','<b>').replace('~/','</b>').replace("||||-","<h4>").replace('-||||','</h4>').replace("|||-","<h3>").replace('-|||','</h3>').replace("||-","<h2>").replace('-||','</h2>').replace("|-","<h1>").replace('-|','</h1>').replace('/*','<small>').replace('*/','</small>').replace('^*','<sup>*</sup>') +'<br/>'
 		
-		if '(yes/no)' in line or '(1/2)' in line or '(1/2/3)' in line:
+		if '(Yes/No)' in line or '(1/2)' in line or '(1/2/3)' in line or 'start?' in line:
 			isQn = True
+			print "Question : "+content
 			return isQn,content
 		
-		if 'Mobile Expenditure is ' in line:
+		if '~EndOfResult!' in line:
+			print "EndOfResult : "+content
 			isQn = False
 			return isQn,content
-
+		
 def giveAnswer(clips_process,answer):
+	if answer == 'No Preference':
+		answer = 'n'
+	elif answer == 'Start' or answer == 'Yes':
+		answer = 'y'
 	print answer
 	clips_process.stdin.write(answer+'\n')
 
